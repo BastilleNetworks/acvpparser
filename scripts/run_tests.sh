6d37a7b31446f417fe1e3a640c13554d543248a7
David Skoog <dwskoog@bastille.io>
2025-08-13 12:01:10 -0400

    WIP: runner script

---
+++ scripts/run_tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ACVP_DIR=$(realpath "${SCRIPTS_DIR}"/..)
+cd "${ACVP_DIR}"
+
+BUNDLE_DIR=$(realpath "${1}")
+
+make clean
+make -s openssl
+
+for GIVEN_FILE in "${BUNDLE_DIR}"/given/**/*.json; do
+  RAW_FILE=$(echo "${GIVEN_FILE}" | sed -e s/given/raw/)
+  echo RUNNING: ./acvp-parser "${GIVEN_FILE}" "${RAW_FILE}"
+  set +e
+  ./acvp-parser "${GIVEN_FILE}" "${RAW_FILE}"
+  set -e
+done
+
+tree "${BUNDLE_DIR}"
